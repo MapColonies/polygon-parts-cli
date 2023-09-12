c80671d538d18f06841e41d2086a4ac2b703ed02
ronenk1 <ronenk1@rnd-hub.com>
2023-09-12 15:52:08 +0300

    feat: adding more data validations + refactoring

--- src/dataSource.js
+++ src/dataSource.js
@@ -63,75 +63,80 @@ class FileToDB {
         .on("data", async (row) => {
           linesCounter += 1;
           const isValidRow = this.validateData(row); // checking mandatory fields
-          if (isValidRow) {
-            const polygons = this.multiPolygon2Polygons(
-              row[this.columnMappedKeys["wkt"]]
-            );
-            let polygonCounter = 0;
-            for (const polygon of polygons) {
-              polygonCounter += 1;
-              console.log(
-                `Processing line number ${linesCounter} -- polygon ${polygonCounter}/${polygons.length}`
-              );
-              const polygonParts = {
-                recordId: row[this.columnMappedKeys["recordid"]],
-                productId: row[this.columnMappedKeys["productid"]]
-                  ? row[this.columnMappedKeys["productid"]]
-                  : "unknown",
-                productName: row[this.columnMappedKeys["productname"]]
-                  ? row[this.columnMappedKeys["productname"]]
-                  : "unknown",
-                productVersion: "1.0",
-                productType: PRODUCT_TYPE_MAPPING[row[this.columnMappedKeys["producttype"]].toLowerCase()],
-                imageName: row[this.columnMappedKeys["imagename"]]
-                  ? row[this.columnMappedKeys["imagename"]]
-                  : undefined,
-                //todo - start date taken from end
-                sourceStartDateUtc: row[
-                  this.columnMappedKeys["sourcedateend"]
-                ].replaceAll("/", "-"),
-                sourceEndDateUtc: row[
-                  this.columnMappedKeys["sourcedateend"]
-                ].replaceAll("/", "-"),
-                //todo - min res taken from max
-                minResolutionDegree: parseFloat(
-                  row[this.columnMappedKeys["maxresolutiondeg"]]
-                ),
-                maxResolutionDegree: parseFloat(
-                  row[this.columnMappedKeys["maxresolutiondeg"]]
-                ),
-                //todo - min res taken from max
-                minResolutionMeter: parseFloat(
-                  row[this.columnMappedKeys["maxresolutionmeter"]]
-                ),
-                maxResolutionMeter: parseFloat(
-                  row[this.columnMappedKeys["maxresolutionmeter"]]
-                ),
-                minHorizontalAccuracyCe90: parseFloat(
-                  row[this.columnMappedKeys["minhorizontalaccuracyce90"]]
-                ),
-                sensors: row[this.columnMappedKeys["sensors"]],
-                region: row[this.columnMappedKeys["region"]]
-                  ? row[this.columnMappedKeys["region"]]
-                  : "unknown",
-                classification:
-                  CLASSIFICATION_MAPPING[
-                    row[this.columnMappedKeys["classification"]].toLowerCase()
-                  ],
-                description: row[this.columnMappedKeys["description"]]
-                  ? row[this.columnMappedKeys["description"]]
-                  : "",
-                geom: polygon,
-              };
-              console.log(
-                `Insert to DB line number ${linesCounter} -- polygon ${polygonCounter}/${polygons.length}`
-              );
-              await this.dbClient.insertPolygonPart(polygonParts);
-            }
-          } else {
+          if (!isValidRow) {
             console.error(
               `failed data validation for line ${linesCounter}, will skip this line`
             );
+            return;
+          }
+
+          // pass validation for current row in csv and will process data to db for current row
+          const polygons = this.multiPolygon2Polygons(
+            row[this.columnMappedKeys["wkt"]]
+          );
+          let polygonCounter = 0;
+          for (const polygon of polygons) {
+            polygonCounter += 1;
+            console.log(
+              `Processing line number ${linesCounter} -- polygon ${polygonCounter}/${polygons.length}`
+            );
+            const polygonParts = {
+              recordId: row[this.columnMappedKeys["recordid"]],
+              productId: row[this.columnMappedKeys["productid"]]
+                ? row[this.columnMappedKeys["productid"]]
+                : "unknown",
+              productName: row[this.columnMappedKeys["productname"]]
+                ? row[this.columnMappedKeys["productname"]]
+                : "unknown",
+              productVersion: "1.0",
+              productType:
+                PRODUCT_TYPE_MAPPING[
+                  row[this.columnMappedKeys["producttype"]].toLowerCase()
+                ],
+              imageName: row[this.columnMappedKeys["imagename"]]
+                ? row[this.columnMappedKeys["imagename"]]
+                : undefined,
+              //todo - start date taken from end
+              sourceStartDateUtc: row[
+                this.columnMappedKeys["sourcedateend"]
+              ].replaceAll("/", "-"),
+              sourceEndDateUtc: row[
+                this.columnMappedKeys["sourcedateend"]
+              ].replaceAll("/", "-"),
+              //todo - min res taken from max
+              minResolutionDegree: parseFloat(
+                row[this.columnMappedKeys["maxresolutiondeg"]]
+              ),
+              maxResolutionDegree: parseFloat(
+                row[this.columnMappedKeys["maxresolutiondeg"]]
+              ),
+              //todo - min res taken from max
+              minResolutionMeter: parseFloat(
+                row[this.columnMappedKeys["maxresolutionmeter"]]
+              ),
+              maxResolutionMeter: parseFloat(
+                row[this.columnMappedKeys["maxresolutionmeter"]]
+              ),
+              minHorizontalAccuracyCe90: parseFloat(
+                row[this.columnMappedKeys["minhorizontalaccuracyce90"]]
+              ),
+              sensors: row[this.columnMappedKeys["sensors"]],
+              region: row[this.columnMappedKeys["region"]]
+                ? row[this.columnMappedKeys["region"]]
+                : "unknown",
+              classification:
+                CLASSIFICATION_MAPPING[
+                  row[this.columnMappedKeys["classification"]].toLowerCase()
+                ],
+              description: row[this.columnMappedKeys["description"]]
+                ? row[this.columnMappedKeys["description"]]
+                : "",
+              geom: polygon,
+            };
+            console.log(
+              `Insert to DB line number ${linesCounter} -- polygon ${polygonCounter}/${polygons.length}`
+            );
+            await this.dbClient.insertPolygonPart(polygonParts);
           }
         })
         .on("end", () => {
@@ -141,8 +146,8 @@ class FileToDB {
   };
 
   /**
- * Generate hashed object (columnMappedKeys) that map csv column to numeral value so the column order between csv files not important
- */
+   * Generate hashed object (columnMappedKeys) that map csv column to numeral value so the column order between csv files not important
+   */
   constructCsvHeaders = async () => {
     return new Promise((resolve, reject) => {
       let enumerableHeaders = {};
@@ -193,32 +198,42 @@ class FileToDB {
 
     if (!row[this.columnMappedKeys["recordid"]]) {
       isValid = false;
-      this.printValidationError('recordId');
+      this.printValidationError("recordId");
     }
 
     if (!row[this.columnMappedKeys["wkt"]]) {
       isValid = false;
-      this.printValidationError('wkt');
+      this.printValidationError("wkt");
+    } else {
+      const wkt = row[this.columnMappedKeys["wkt"]];
+      const geoType = wkt.split(" ")[0];
+      if (geoType !== "POLYGON" && geoType !== "MULTIPOLYGON") {
+        isValid = false;
+        console.error(
+          `wkt should be consist of Polygon or MultiPolygon only types, provided wkt: "${geoType}"`
+        );
+      }
     }
+
 
     if (!row[this.columnMappedKeys["sourcedateend"]]) {
       isValid = false;
-      this.printValidationError('sourceDateEnd');
+      this.printValidationError("sourceDateEnd");
     }
 
     if (!row[this.columnMappedKeys["maxresolutiondeg"]]) {
       isValid = false;
-      this.printValidationError('maxResolutionDeg');
+      this.printValidationError("maxResolutionDeg");
     }
 
     if (!row[this.columnMappedKeys["maxresolutionmeter"]]) {
       isValid = false;
-      this.printValidationError('maxResolutionMeter');
+      this.printValidationError("maxResolutionMeter");
     }
 
     if (!row[this.columnMappedKeys["minhorizontalaccuracyce90"]]) {
       isValid = false;
-      this.printValidationError('minHorizontalAccuracyCe90');
+      this.printValidationError("minHorizontalAccuracyCe90");
     }
 
     return isValid;
@@ -230,7 +245,6 @@ class FileToDB {
     );
   };
 
-
   multiPolygon2Polygons = (wkt) => {
     const geoJson = geoParse(wkt);
     if (geoJson["type"] === "Polygon") return geoJson;
@@ -241,8 +255,6 @@ class FileToDB {
     return polygonsArray;
   };
 }
-
-
 
 module.exports = {
   FileToDB,
