2d3c5fdfc023aa6e6f54cda5f865796196dfccf9
vitaligi <54726763+vitaligi@users.noreply.github.com>
2023-11-13 00:47:08 +0200

    fix: region is not required

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
 export const SUPPORTED_GEO_TYPES = ['MULTIPOLYGON', 'POLYGON'];
 
-export const OPTIONAL_FIELDS = ['minHorizontalAccuracyCE90', 'maxHorizontalAccuracyCE90', 'description', 'imageName', 'minResolutionMeter', 'minResolutionDeg', 'sourceDateStart'] as const;
+export const OPTIONAL_FIELDS = ['minHorizontalAccuracyCE90', 'maxHorizontalAccuracyCE90', 'description', 'imageName', 'minResolutionMeter', 'minResolutionDeg', 'sourceDateStart', 'region'] as const;
 export const FIXED_FIELDS = ['productVersion'] as const;
-export const REQUIRED_FIELDS = ['productId', 'recordId', 'classification', 'productType', 'srsName', 'maxResolutionMeter', 'sensors', 'productName', 'sourceDateEnd', 'maxResolutionDeg', 'region', 'geom'] as const;
+export const REQUIRED_FIELDS = ['productId', 'recordId', 'classification', 'productType', 'srsName', 'maxResolutionMeter', 'sensors', 'productName', 'sourceDateEnd', 'maxResolutionDeg', 'geom'] as const;
 export const ALL_FIELDS = [...REQUIRED_FIELDS, ...FIXED_FIELDS, ...OPTIONAL_FIELDS] as const;
 
 export const VALIDATION_ERRORS = {
