7cd461ef2dcd19e97d6660f12bb5a5e62b6a702a
vitaligi <54726763+vitaligi@users.noreply.github.com>
2023-11-29 22:50:36 +0200

    style: list const items vertically

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,58 @@
-export const SUPPORTED_GEO_TYPES = ['MULTIPOLYGON', 'POLYGON'];
+export const SUPPORTED_GEO_TYPES = [
+  'MULTIPOLYGON',
+  'POLYGON'
+];
 
-export const OPTIONAL_FIELDS = ['minHorizontalAccuracyCE90', 'maxHorizontalAccuracyCE90', 'description', 'imageName', 'minResolutionMeter', 'minResolutionDeg', 'sourceDateStart', 'region', 'productVersion', 'productId', 'classification', 'sensors', 'productName'] as const;
-export const REQUIRED_FIELDS = ['recordId', 'productType', 'srsName', 'maxResolutionMeter', 'sourceDateEnd', 'maxResolutionDeg', 'geom'] as const;
+export const OPTIONAL_FIELDS = [
+  'minHorizontalAccuracyCE90',
+  'maxHorizontalAccuracyCE90',
+  'description',
+  'imageName',
+  'minResolutionMeter',
+  'minResolutionDeg',
+  'sourceDateStart',
+  'region',
+  'productVersion',
+  'productId',
+  'classification',
+  'sensors',
+  'productName'
+] as const;
+
+export const REQUIRED_FIELDS = [
+  'recordId',
+  'productType',
+  'srsName',
+  'maxResolutionMeter',
+  'maxResolutionDeg',
+  'sourceDateEnd',
+  'geom'
+] as const;
+
 export const ALL_FIELDS = [...REQUIRED_FIELDS, ...OPTIONAL_FIELDS] as const;
 
-export const INSERT_POLYGON_PART_FIELDS = ['recordId', 'productId', 'productName', 'productVersion', 'sourceDateStart', 'sourceDateEnd', 'minResolutionDeg', 'maxResolutionDeg', 'minResolutionMeter', 'maxResolutionMeter', 'minHorizontalAccuracyCE90', 'maxHorizontalAccuracyCE90', 'sensors', 'region', 'classification', 'description', 'geom', 'imageName', 'productType', 'srsName'];
+export const INSERT_POLYGON_PART_FIELDS = [
+  'recordId',
+  'productId',
+  'productName',
+  'productVersion',
+  'sourceDateStart',
+  'sourceDateEnd',
+  'minResolutionDeg',
+  'maxResolutionDeg',
+  'minResolutionMeter',
+  'maxResolutionMeter',
+  'minHorizontalAccuracyCE90',
+  'maxHorizontalAccuracyCE90',
+  'sensors',
+  'region',
+  'classification',
+  'description',
+  'geom',
+  'imageName',
+  'productType',
+  'srsName'
+];
 
 export const VALIDATION_ERRORS = {
   mandatoryField: 'missing a mandatory field',
