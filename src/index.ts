71ff56c394388b696f77b118b2c6f86175b50fdd
vitaligi <54726763+vitaligi@users.noreply.github.com>
2024-02-05 19:29:53 +0200

    refactor: typescript and cleanup (#3)
    * refactor: typescript & cleanup
    
    * refactor: add env configs
    
    * refactor: cleanup pool resources
    
    * refactor: pr comments
    
    * refactor: validations & error handling
    
    * style: missing new line
    
    * refactor: replace to @terraformer/wkt
    
    * fix: cr comments
    
    * refactor: improve type importing
    
    * style: remove semicolons
    
    * refactor: wip
    
    * refactor: add csv config
    
    * style: line at the end of file
    
    * refactor: cr comments & wip
    
    * refactor: cleanup & db error handling
    
    * style: removed comments
    
    * fix: region is not required
    
    * fix: productVersion is optional
    
    * refactor: move to constants.ts
    
    * fix: async operations should be awaited
    
    * refactor: line up with nullable columns
    
    * style: list const items vertically
    
    * refactor: null support for optional fields
    
    * fix: missed error handling
    
    * bug: missing dot
    
    * refactor: wip
    
    * refactor: merge self-overlapping multipart polygons
    
    * refactor: call for stored proc and terminology
    
    * feat: clean termination
    
    * docs: update
    
    * style: emojis
    
    * feat: support wkt insert geom for postgis prior to 3.0
    
    * refactor: consistent field names
    
    * refactor: remove console.logs
    
    * refactor: cr
    
    * refactor: type imports
    
    * refactor: application name
    
    * refactor: support fallback fields
    
    * fix: handle undefined only
    
    * refactor: rename func

---
+++ src/index.ts
@@ -0,0 +1,79 @@
+import config from 'config';
+import yargs, { showHelp } from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import { name as packageName, version as packageVersion } from '../package.json';
+import { CSVToDB } from './csvToDB';
+import { DBProvider } from './pg';
+import type { PGConfig } from './types';
+import { getErrorMessage } from './utilities';
+
+const argv = yargs(hideBin(process.argv))
+  .usage('insert polygon part raw data into db')
+  .option('i', {
+    alias: 'input',
+    describe: 'input file (csv)',
+    type: 'string'
+  })
+  .version(packageVersion)
+  .help(true)
+  .parseSync();
+
+if (argv.i) {
+  const filePath = argv.i;
+
+  const dbConfig = config.get<PGConfig>('pgConfig');
+  const applicationName = dbConfig.applicationName ?? packageName;
+  const adminProvider = new DBProvider({ ...dbConfig, ...{ applicationName: `${applicationName}-admin`, pool: { min: 0, max: 1 } } });
+  const dbProvider = new DBProvider({ ...dbConfig, ...{ applicationName: applicationName } }); // TODO: inject
+
+  let shutdown = true;
+
+  // https://github.com/brianc/node-postgres/issues/773
+  // to cleanup pg resources an admin client terminating ongoing queries is currently the only option
+  // this may require setting additional access to the user - https://www.postgresql.org/docs/12/functions-admin.html#FUNCTIONS-ADMIN-SIGNAL
+  const gracefulShutdown = async () => {
+    if (shutdown) {
+      shutdown = false;
+      try {
+        if (dbProvider.backendPID)
+          await adminProvider.cancel(dbProvider.backendPID);
+      } catch (err) {
+        const errMessage = getErrorMessage(err);
+        console.error(`Failed to cleanup DB resources: ${errMessage}`);
+      }
+    }
+  }
+
+  process.on('SIGTERM', async () => {
+    await gracefulShutdown();
+  });
+
+  process.on('SIGINT', async () => {
+    await gracefulShutdown();
+  });
+
+  const polygonPartsUploader = new CSVToDB(filePath, dbProvider);
+
+  (async () => {
+    let exitCode: number = 1;
+    try {
+      console.log(`Start processing file 🎬: ${filePath}`); // TODO: replace with @map-colonies/js-logger
+      const summary = await polygonPartsUploader.csvToPg();
+      console.log('Processing finished 🏁');
+      console.log(`Summary 📋:
+Total rows processed: ${summary.rowsProcessed}
+Polygons processed: ${summary.polygonsProcessed}
+    `);
+      exitCode = 0;
+    } catch (err) {
+      const errMessage = getErrorMessage(err);
+      console.error(`🥴 Processing failed with an error: ${errMessage}`);
+    } finally {
+      process.exit(exitCode);
+    }
+  })();
+}
+
+else {
+  showHelp();
+}
---
+++ src/utilities.ts
@@ -0,0 +1,23 @@
+import type { ExtractItem } from "./types";
+
+export const getErrorMessage = (err: unknown): string => {
+    let message: string | undefined;
+    if (err instanceof Error) message = err.message;
+    else if (typeof err === 'string') message = err;
+    else message = String(err);
+    return message;
+}
+
+export const isInArray = <T extends string>(value: string, array: Readonly<Array<T>>): value is T => {
+    return !!array.find(item => item === value);
+}
+
+export const getValue = <T extends any[], K extends number | undefined>(row: T, field: K): ExtractItem<T, K, null> => {
+    let value: any | null;
+    if (field === undefined)
+        value = null;
+    else
+        value = row.at(field);
+    if (value === undefined) throw new RangeError('Field was not found in the row');
+    return value;
+}
